033396cde9febbe4710054b6a1d1726489610f9f
theobdt <theo.bodrito@hotmail.fr>
2020-02-25 11:27:21 +0100

    Download model automatically from google drive

---
+++ download_model.sh
@@ -0,0 +1,9 @@
+mkdir -p ckpts
+function gdrive_download () {
+  CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+  rm -rf /tmp/cookies.txt
+}
+gdrive_download 1-9bEAw5bx3htMiFW5uY_unLyCuKn9EBc ckpts/tmp.zip
+unzip ckpts/tmp.zip -d ckpts/
+rm ckpts/tmp.zip
